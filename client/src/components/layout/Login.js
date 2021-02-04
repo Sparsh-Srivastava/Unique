e57e24a2b9fb223b815771bb6397093d57dadf52
raghav13901 <raghavkhatoria42@gmail.com>
2021-02-04 06:48:26 +0530

    UI changed

--- client/src/components/layout/Login.js
+++ client/src/components/layout/Login.js
@@ -4,7 +4,8 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { loginUser } from "../../actions/authActions";
 import classnames from "classnames";
-
+import img from "../imgs/undraw_authentication_fsn5 (2).svg";
+import styles from "../styles/Login.module.css";
 class Login extends Component{
     constructor(){
         super()
@@ -48,12 +49,24 @@ class Login extends Component{
     render(){
         const {email, password, errors} = this.state;
         return(
-            <div className="form-box">
-            <form className="login-form" onSubmit={this.onSubmit}>
-                <h2>Login</h2>
-                <hr/>
-                <div className="form-group">
-                    <input type="email" 
+          <div className={`${styles.logInDoc}`}> 
+            <div className={`row ${styles.row} text-center`}>
+            <div className={`col-lg-8 ${styles.colLg8}`}>
+              <img alt="..." src={img} className="logImg" />
+            </div>
+            <div className={`col-lg-4 ${styles.colLg4}`}>
+              <div className={`shadow-lg  p-3  ${styles.rounded}`}>
+                <h2>Log In</h2>
+                <div className="p-3">
+                  <form onSubmit={this.onSubmit}>
+                    <div className={`row ${styles.row}`}>
+                      <div className={`col-sm-2 ${styles.colSm2}`}>
+                        <h6 className="head">
+                          <i className="fas fa-envelope fa-lg"></i>
+                        </h6>
+                      </div>
+                      <div className={`col-sm-10 ${styles.colSm10}`}>
+                        <input type="email" 
                            id="email"
                            placeholder="Email Address" 
                            value={email} 
@@ -62,13 +75,20 @@ class Login extends Component{
                            className={classnames("form-control", {
                             invalid: errors.email || errors.emailnotfound
                           })}/>
-                          <span className="red-text">
+                          <span className={`red-text ${styles.error}`}>
                             {errors.email}
                             {errors.emailnotfound}
                         </span>
-                </div>
-                <div className="form-group">
-                    <input type="password"  
+                      </div>
+                    </div>
+                    <div className={`row ${styles.row}`}>
+                      <div className={`col-sm-2 ${styles.colSm2}`}>
+                        <h6 className="head">
+                          <i className="fas fa-key fa-lg"></i>
+                        </h6>
+                      </div>
+                      <div className={`col-sm-10 ${styles.colSm10}`}>
+                          <input type="password"  
                            id="password" 
                            placeholder="Password" 
                            value={password} 
@@ -76,19 +96,23 @@ class Login extends Component{
                            onChange={this.onChange}
                            className={classnames("form-control", {
                             invalid: errors.password || errors.passwordincorrect })}
-                            />
-                    <span className="red-text">
-                        {errors.password}
-                        {errors.passwordincorrect}
-                    </span>
+                          />
+                          <span className={`para ${styles.error}`}>
+                            {errors.password}
+                            {errors.passwordincorrect}
+                         </span>
+                      </div>
+                    </div>
+                    <div className="form-group">
+                     <button type="submit" className={`btn btn-primary btn-block btn-lg ${styles.logInDocBtn}`}>Login</button>
+                    </div>
+                    <div className="text-center">Don't have an account? <Link to="/register">Register</Link></div>
+                  </form>
+                  <p className={`para ${styles.error}`}>{this.state.errMessage}</p>
                 </div>
-                <div className="form-group">
-                    <button type="submit" className="btn btn-primary btn-block btn-lg">Login</button>
-                </div>
-                <div className="text-center">Don't have an account? <Link to="/register">Register</Link></div>
-
-
-            </form>
+              </div>
+            </div>
+          </div>
         </div>
         )
     }
--- client/src/components/layout/Register.js
+++ client/src/components/layout/Register.js
@@ -4,6 +4,9 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { registerUser } from "../../actions/authActions";
 import classnames from "classnames";
+import {Col,Row,Container} from 'react-bootstrap';
+import img from "../imgs/undraw_sign_in_e6hj.svg";
+import styles from "../styles/Register.module.css";
 
 class Register extends Component{
     constructor(){
@@ -13,7 +16,11 @@ class Register extends Component{
             email:"",
             password:"",
             password2:"",
-            errors:{}
+            errors:{},
+            checked:"",
+            subject:"",
+            sclass:"",
+            school:""
         }   
     }
     componentDidMount() {
@@ -33,6 +40,10 @@ class Register extends Component{
     onChange = e => {
         this.setState({[e.target.id]:e.target.value})
     }
+    
+    handleCheck = e =>{
+      this.setState({checked:!this.state.checked});
+    }
 
     onSubmit = e => {
         e.preventDefault();
@@ -47,79 +58,208 @@ class Register extends Component{
         this.props.registerUser(newUser, this.props.history); 
     }
 
+    showSub(){
+    }
+
+
+
     render(){
-        const {name, email, password, password2 ,errors} = this.state;
-
-        return(
-            <div className="form-box ">
-            <form className="signup-form"  onSubmit={this.onSubmit}>
-
-               <div><Link to="/"><i className="fa fa-arrow-circle-left  "></i> Back to Home</Link></div>
-
-                <h2>Register</h2>
-                <hr/>
-                <div className="form-group">
+        const {name, email, password, password2 ,errors ,checked ,sclass ,school} = this.state;
+        const change = this.onChange;
+        function msg(a){
+          if(a){
+            return(
+              <div className="form-group">
+                <Row  className="align-items-center">
+                  <Col sm={2} className={`text-center ${styles.col}`}>
+                    <i className="fas fa-book fa-lg"></i>
+                  </Col>
+                  <Col sm={10} className={styles.col}>
                     <input type="text" 
-                           id="name" 
-                           placeholder="Name" 
-                           value={name}
-                           error={errors.name} 
-                           onChange={this.onChange} 
-                           className={classnames("form-control", {
-                            invalid: errors.name
+                          id="subject" 
+                          placeholder="Enter Subject" 
+                          value={name}
+                          error={errors.subject} 
+                          onChange={change} 
+                          className={classnames("form-control", {
+                            invalid: errors.subject
                           })}/> 
-                        <span className="red-text">{errors.name}</span>
+                    <span className="red-text">{errors.subject}</span>
+                  </Col>
+                </Row>
+              </div>
+            );
+          }
+        }
+        return(
+          <Container fluid className={styles.signupDoc}>
+            <Row className="">
+              <Col lg={8} className={styles.imgCol}>
+                <img alt="..." src={img} />
+              </Col>
+              <Col lg={4} className={styles.formCol}>
+                      <div className="form-box ">
+            
+                      <form className={`signup-form ${styles.rounded} shadow-lg`}  onSubmit={this.onSubmit}>
+                          <h2 className="text-center">Register</h2>
+                          <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel" dataInterval="false" style={{height:"100%"}}>
+                            <div className="carousel-inner">
+                              <div className="carousel-item active">
+                                <div className="form-group">
+                                  <Row  className="align-items-center">
+                                    <Col sm={2} className={`text-center ${styles.col}`}>
+                                      <i className="fas fa-signature fa-lg"></i>
+                                    </Col>
+                                    <Col sm={10} className={styles.col}>
+                                      <input type="text" 
+                                            id="name" 
+                                            placeholder="Name" 
+                                            value={name}
+                                            error={errors.name} 
+                                            onChange={this.onChange} 
+                                            className={classnames("form-control", {
+                                              invalid: errors.name
+                                            })}/> 
+                                      <span className="red-text">{errors.name}</span>
+                                    </Col>
+                                  </Row>
+                                </div>
 
-                </div>
+                                <div className="form-group">
+                                  <Row  className="align-items-center">
+                                    <Col sm={2} className={`text-center ${styles.col}`}>
+                                      <i className="fas fa-envelope fa-lg"></i>
+                                    </Col>
+                                    <Col sm={10} className={styles.col}>
+                                      <input type="email" 
+                                            id="email" 
+                                            placeholder="Email Address" 
+                                            value={email}
+                                            error={errors.email}
+                                            onChange={this.onChange} 
+                                            className={classnames("form-control", {
+                                              invalid: errors.email
+                                            })}/>
+                                      <span className="red-text">{errors.email}</span>
+                                    </Col>
+                                  </Row>  
+                                </div>
 
-                <div className="form-group">
-                    <input type="email" 
-                           id="email" 
-                           placeholder="Email Address" 
-                           value={email}
-                           error={errors.email}
-                           onChange={this.onChange} 
-                           className={classnames("form-control", {
-                            invalid: errors.email
-                          })}/>
-                    <span className="red-text">{errors.email}</span>
-                </div>
+                                <div className="form-group">
+                                  <Row  className="align-items-center">
+                                    <Col sm={2} className={`text-center ${styles.col}`}>
+                                      <i className="fas fa-key fa-lg"></i>
+                                    </Col>
+                                    <Col sm={10} className={styles.col}>
+                                      <input type="password" 
+                                            id="password" 
+                                            placeholder="Password" 
+                                            value={password}
+                                            error= {errors.password} 
+                                            onChange={this.onChange}
+                                            className={classnames("form-control", {
+                                              invalid: errors.password
+                                            })} />
+                                      <span className="red-text">{errors.password}</span>   
+                                    </Col>
+                                  </Row>  
+                                </div>
 
-                <div className="form-group">
-                    <input type="password" 
-                           id="password" 
-                           placeholder="Password" 
-                           value={password}
-                           error= {errors.password} 
-                           onChange={this.onChange}
-                           className={classnames("form-control", {
-                            invalid: errors.password
-                          })} />
-                    <span className="red-text">{errors.password}</span>                     
-                </div>
+                                <div className="form-group">
+                                  <Row  className="align-items-center">
+                                    <Col sm={2} className={`text-center ${styles.col}`}>
+                                      <i className="fas fa-check-circle fa-lg"></i>
+                                    </Col>
+                                    <Col sm={10} className={styles.col}>
+                                      <input type="password" 
+                                            id="password2"
+                                            placeholder="Confirm Password" 
+                                            value={password2} 
+                                            error={errors.password}
+                                            onChange={this.onChange}
+                                            className={classnames("form-control", {
+                                              invalid: errors.password2
+                                            })} />
+                                      <span className="red-text">{errors.password2}</span>   
+                                    </Col>
+                                  </Row>  
+                                </div>
+                              </div>
+                              <div className="carousel-item" style={{height:"100%"}}>
+                                  <Row style={{marginTop:"5%"}}>
+                                      <Col sm={2} className={`text-center ${styles.col}`}>
+                                        <input type="checkbox" onChange={this.handleCheck} id="checked" />
+                                      </Col>
+                                      <Col sm={10} className={`${styles.col} text-left`}>
+                                        <p>Are you a Teacher?</p>
+                                      </Col>
+                                  </Row>
+                                {msg(this.state.checked)}
+                                <div className="form-group">
+                                  <Row  className="align-items-center">
+                                    <Col sm={2} className={`text-center ${styles.col}`}>
+                                      <i className="fas fa-chalkboard-teacher fa-lg"></i>
+                                    </Col>
+                                    <Col sm={10} className={styles.col}>
+                                      <input type="text" 
+                                            id="sclass"
+                                            placeholder="Enter Class" 
+                                            value={sclass} 
+                                            error={errors.sclass}
+                                            onChange={this.onChange}
+                                            className={classnames("form-control", {
+                                              invalid: errors.sclass
+                                            })} />
+                                      <span className="red-text">{errors.sclass}</span>   
+                                    </Col>
+                                  </Row> 
+                                </div>
+                                <div className="form-group">
+                                  <Row  className="align-items-center">
+                                    <Col sm={2} className={`text-center ${styles.col}`}>
+                                      <i className="fas fa-school fa-lg"></i>
+                                    </Col>
+                                    <Col sm={10} className={styles.col}>
+                                      <input type="text" 
+                                            id="school"
+                                            placeholder="Enter School Name" 
+                                            value={school} 
+                                            error={errors.school}
+                                            onChange={this.onChange}
+                                            className={classnames("form-control", {
+                                              invalid: errors.school
+                                            })} />
+                                      <span className="red-text">{errors.school}</span>   
+                                    </Col>
+                                  </Row>
+                                </div>
+                                  
+                              </div>
+                            </div>
+                            <a className={`${styles.prev} carousel-control-prev`} href="#carouselExampleControls" role="button" data-bs-slide="prev">
+                              <span className={`${styles.prevIcon}  carousel-control-prev-icon`} aria-hidden="true"></span>
+                            </a>
+                            <a className={`${styles.next} carousel-control-next`} href="#carouselExampleControls" role="button" data-bs-slide="next">
+                              <span className={`${styles.nextIcon}  carousel-control-next-icon`} aria-hidden="true"></span>
+                            </a>
 
-                <div className="form-group">
-                    <input type="password" 
-                           id="password2"
-                           placeholder="Confirm Password" 
-                           value={password2} 
-                           error={errors.password}
-                           onChange={this.onChange}
-                           className={classnames("form-control", {
-                            invalid: errors.password2
-                          })} />
-                    <span className="red-text">{errors.password2}</span>
-                </div>
-                <div className="form-group">
-                    <button type="submit" className="btn btn-primary btn-block btn-lg">Sign Up</button>
-                </div>
-                <div className="text-center">Already have an account? <Link to="/login">Login here</Link></div>
+                          </div>
 
-            </form>
-        </div>
+                          <div className="form-group text-center">
+                            <button type="submit" className={`btn btn-primary btn-lg ${styles.signInDocBtn}`}>Sign Up</button>
+                          </div>
+                          <div className="text-center">Already have an account? <Link to="/login">Login here</Link></div>
+                          
+
+                      </form>
+                  </div>
+              </Col>
+            </Row>
+        </Container>
         )
     }
 }
+
 
 Register.propTypes = {
     registerUser: PropTypes.func.isRequired,
